196b62f165762ad908709be10675d4e5cb139bad
Tobi Tselios <tobi.tselios@gmail.com>
2019-11-16 19:02:16 +0000

    Storyboard configured

---
+++ CarsForSale/CarCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CarCollectionViewCell.swift
+//  CarsForSale
+//
+//  Created by Tobi Kuyoro on 16/11/2019.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class CarCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var carNameLabel: UILabel!
+    @IBOutlet weak var carImage: UIImageView!
+}
---
+++ CarsForSale/CarViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CarViewController.swift
+//  CarsForSale
+//
+//  Created by Tobi Kuyoro on 16/11/2019.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class CarViewController: UIViewController {
+
+    @IBOutlet weak var carNameLabel: UILabel!
+    @IBOutlet weak var carImage: UIImageView!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
